4bed19a213bc6d8fa886e04fe814d91c47b36be2
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-10 15:53:03 +0200

    // variables

--- Assets/script/scriptcube.cs
+++ Assets/script/scriptcube.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class ScriptCube : MonoBehaviour
 {
+
+    // variables 
+
+
+    string coleur = "Rouge";
+    int intensité = 100;
+    bool connect = true;
+    float volt = 200.121f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        print ("test 0 script cube");
+        print ("test 1 script cube");
+        print ("test 2 script cube");
     }
 
     // Update is called once per frame
