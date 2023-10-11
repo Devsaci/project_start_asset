fa53ae5a6c2c6f3d2258bffffc21e760377bc59c
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-11 17:38:32 +0200

    Destroy(other.gameObject);

--- Assets/script/scriptcube.cs
+++ Assets/script/scriptcube.cs
@@ -54,7 +54,7 @@ public class ScriptCube : MonoBehaviour
     private void OnTriggerEnter(Collider other) 
     {
        
-        Destroy(other);
+        Destroy(other.gameObject);
         
     }
 
