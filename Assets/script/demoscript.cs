ea5e89add7bd1389bbceb2bbf67fe270e4bdcde2
Devsaci <46720185+Devsaci@users.noreply.github.com>
2023-10-12 11:14:04 +0200

    print("script 1");

--- Assets/script/demoscript.cs
+++ Assets/script/demoscript.cs
@@ -6,12 +6,13 @@ public class demoscript : MonoBehaviour
 {
 
 
-    public string col;
+    public string col = "Script 2";
     public int Vol;
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("script 1");
+        print("Script 2");
     }
 
     // Update is called once per frame
