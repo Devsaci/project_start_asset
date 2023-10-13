f2d717188598f3d8420870c93535a3c2882cd761
saci zakaria <sacideveloppeur@gmail.com>
2023-10-13 09:51:19 +0200

    gameObject.SetActive(false)bis

--- Assets/script/demoscript.cs
+++ Assets/script/demoscript.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class demoscript : MonoBehaviour
 {
-
+    public Rigidbody rd;
 
     public string col = "Script 2";
     public int Vol;
     // Start is called before the first frame update
     void Start()
     {
-       print("script 1");
-       print("Script 2");
-        Debug.Log("message  : " + gameObject.name);
+        print("script 1");
+                Debug.Log("message  : " + gameObject.name);
+        gameObject.SetActive(false);
+        /* if (Input.GetMouseButtonDown(1))
+         {
+             rd.gameObject.SetActive(false);
+         }*/
+        print("Script 2");
+        //gameObject.SetActive(true);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       // print("script 1");
+        // print("script 1");
     }
 }
