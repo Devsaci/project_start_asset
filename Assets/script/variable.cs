1a2f0d692f42ee1584e9f559f880cadc6edc7ed6
saci zakaria <sacideveloppeur@gmail.com>
2023-10-13 10:52:41 +0200

    print("mon age50 est  : " + age);

--- Assets/script/variable.cs
+++ Assets/script/variable.cs
@@ -7,12 +7,14 @@ public class variable : MonoBehaviour
 
 
     //Variable 
-    public int age = 50;
+    public int age = 20;
 
     // Start is called before the first frame update
     void Start()
     {
-        print("mon age est  : " + age);
+        print("mon age20 est  : " + age);
+        age =  50;
+        print("mon age50 est  : " + age);
     }
 
     // Update is called once per frame
