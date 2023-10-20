adb5bc3990bc7cafc70ae39b0248bec204d5ab19
saci zakaria <sacideveloppeur@gmail.com>
2023-10-20 15:31:11 +0200

    while (i < 10)

--- Assets/script/Boucles.cs
+++ Assets/script/Boucles.cs
@@ -7,15 +7,21 @@ public class Boucles : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 10; i++) 
-        {
-            print(message: "la valeur de i est :  " + i);
-        }
+        /* for(int i = 0; i < 10; i++) 
+         {
+             print(message: "la valeur de i est :  " + i);
+         }*/
+        int i = 0;
+        while (i < 10)
+        { 
+            i = i + 1; 
+        };
+        print("i vaut : " + i);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
