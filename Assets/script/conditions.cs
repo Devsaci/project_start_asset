bb774f89858b388d38e474c1f22a8441c9abc1bd
saci zakaria <sacideveloppeur@gmail.com>
2023-10-20 10:47:05 +0200

    if (chaleur < 0) /  else if (chaleur > 10)

--- Assets/script/conditions.cs
+++ Assets/script/conditions.cs
@@ -7,20 +7,39 @@ public class conditions : MonoBehaviour
 {
     public bool exemple;
     public Material mat;
-    public Color color = Color.cyan;
+    //public Color color = Color.cyan;
+    public int chaleur;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (exemple) 
-        { 
-            mat.color = Color.red; 
+
+
+        if (chaleur < 0)
+        {
+            mat.color = Color.blue;
         }
-        else 
-        { 
-            mat.color = Color.blue; 
+        else if (chaleur > 10)
+        {
+            mat.color = Color.red;
         }
-        
+       /* else
+        {
+            mat.color = Color.white;
+        }*/
+
+
+
+        /*
+                if (exemple) 
+                { 
+                    mat.color = Color.red; 
+                }
+                else 
+                { 
+                    mat.color = Color.blue; 
+                }*/
+
     }
 
     // Update is called once per frame
