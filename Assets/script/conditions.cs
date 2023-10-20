d2d3428ca913e0f2fdd89da441ca0a19771845d6
saci zakaria <sacideveloppeur@gmail.com>
2023-10-20 10:16:09 +0200

    else

--- Assets/script/conditions.cs
+++ Assets/script/conditions.cs
@@ -7,11 +7,19 @@ public class conditions : MonoBehaviour
 {
     public bool exemple;
     public Material mat;
+    public Color color = Color.cyan;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (exemple == true) { mat.color = Color.red; };
+        if (exemple) 
+        { 
+            mat.color = Color.red; 
+        }
+        else 
+        { 
+            mat.color = Color.blue; 
+        }
         
     }
 
